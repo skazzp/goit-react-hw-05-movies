6e9d98ed6117a0517ff35e77a37fc025e3f66269
skazzp <skazzp@gmail.com>
2022-09-27 23:04:35 +0300

    ?

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,5 +1,24 @@
+// import { useState } from 'react';
+
 const Movies = () => {
-  return <div>Movies</div>;
+  // const [input, setInput] = useState('');
+  return (
+    <div>
+      <form action="">
+        <label htmlFor="">
+          <input
+            type="text"
+            name="search"
+            value={'search'}
+            onChange={() => {}}
+          />
+        </label>
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
 };
 
 export default Movies;
+// "name": "goit-react-hw-05-movies",
+// "homepage": "https://skazzp.github.io/goit-react-hw-05-movies/",
