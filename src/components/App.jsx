d157776e273aff0bc1025b6a72e63f236eae6302
skazzp <skazzp@gmail.com>
2022-09-29 19:15:10 +0300

    folders

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import Navigation from './Navigation/Navigation';
-import Home from './Home/Home';
-import Movies from './Movies/Movies';
-import MovieDetails from './MovieDetails/MovieDetails';
+import Home from '../pages/Home/Home';
+import Movies from '../pages/Movies/Movies';
+import MovieDetails from '../pages/MovieDetails/MovieDetails';
 import { lazy } from 'react';
 const Cast = lazy(() => import('./Cast/Cast'));
 const Reviews = lazy(() => import('./Reviews/Reviews'));
-// import Cast from './Cast/Cast';
-// import Reviews from './Reviews/Reviews';
-// const Movies = lazy(() => import('./Movies/Movies'));
 
 export const App = () => {
   return (
@@ -26,5 +23,6 @@ export const App = () => {
     </>
   );
 };
+// комменты потом почищу, еще буду доделывать
 // "name": "goit-react-hw-05-movies",
 // "homepage": "https://skazzp.github.io/goit-react-hw-05-movies/",
